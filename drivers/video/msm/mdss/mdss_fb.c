9cede36d423fc3496978289c83052b899a64e02c
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-04-16 16:40:49 -0300

    msm: mdss: clean for build

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1071,7 +1071,6 @@ static void mdss_fb_restore_param(struct msm_fb_data_type *mfd)
 }
 
 __PARAM_SYSFS_DEFINITION(hbm, PARAM_HBM_ID)
-__PARAM_SYSFS_DEFINITION(cabc, PARAM_CABC_ID)
 
 static struct device_attribute param_attrs[PARAM_ID_NUM] = {
 	__ATTR(hbm, S_IWUSR | S_IWGRP | S_IRUSR | S_IRGRP, hbm_show, hbm_store),
